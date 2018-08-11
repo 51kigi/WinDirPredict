2f4b32196cd210f5262820ad5b4af06e4b9b99ae
51kigi <kimuragt@gmail.com>
2018-08-12 08:57:11 +0900

    ynoguchiモデルPytrhon実装中

--- model/ynoguchi_v1.py
+++ model/ynoguchi_v1.py
@@ -8,7 +8,7 @@
 w1_path='./w1.csv'
 w2_path='./w2.csv'
 w3_path='./w3.csv'
-output_path='./ynoguchi_pred.csv'
+output_path='../pred/ynoguchi_pred.csv'
 
 import sys
 import numpy as numpy
@@ -28,3 +28,10 @@ w1_df = pd.read_csv(w1_path,sep=',',header=None)
 w2_df=pd.read_csv(w2_path,sep=',',header=None)
 w3_df=pd.read_csv(w2_path,sep=',',header=None)
 
+#各要素について負であれば0に置き換える
+b1=input_df.T.dot(w1_df.values)
+b2=b1.dot(w2_df.values)
+b3=b2.dot(w3_df.values)
+
+#10, 12, 14, 16, 20, 22, 24, 26 を順番に 0,1,2,3,4,5,6,7 に置き換え。（8パターンの分類問題）
+#最大確立のラベルが予想値
